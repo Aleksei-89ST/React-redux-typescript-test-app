6ce26b82636faa66d8f442f029ea268ec2118e69
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-06-03 11:10:35 +0400

    постраничный вывод

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import TodoList from "./components/TodoList";
 import UserList from "./components/UserList";
 
 function App() {
-  return <div>
-    <UserList/>
-  </div>;
+  return (
+    <div>
+      <UserList />
+      <hr />
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
